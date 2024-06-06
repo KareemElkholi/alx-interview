3037d6d8a6360749bd7f9bb909464f89f04d34c3
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-06-06 21:15:53 +0300

    Correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,11 +4,12 @@
 
 def canUnlockAll(boxes):
     """method that determines if all the boxes can be opened"""
+    n = len(boxes)
     visited = []
     queue = [0]
     while queue:
         for key in boxes[queue[0]]:
-            if key not in queue and key not in visited:
+            if key not in queue and key not in visited and key < n:
                 queue.append(key)
         visited.append(queue.pop(0))
-    return len(visited) == len(boxes)
+    return len(visited) == n
