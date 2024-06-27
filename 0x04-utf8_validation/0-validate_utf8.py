dcc24ab9c7238603ef01a33860b6ee37ab434c32
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-06-28 02:54:07 +0300

    All Tasks

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""UTF-8 Validation module"""
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    i = 0
+    l = len(data)
+    while i < l:
+        if data[i] >> 7 == 0:
+            i += 1
+            continue
+        r = 0
+        if data[i] >> 3 == 30:
+            r = 3
+        elif data[i] >> 4 == 14:
+            r = 2
+        elif data[i] >> 5 == 6:
+            r = 1
+        else:
+            return False
+        while (r):
+            r -= 1
+            i += 1
+            if i == l or data[i] >> 6 != 2:
+                return False
+    return True
