32d652562e75b258b159359f37b25cbdc203396a
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-06-28 03:05:42 +0300

    Correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,21 +7,22 @@ def validUTF8(data):
     i = 0
     l = len(data)
     while i < l:
-        if data[i] >> 7 == 0:
+        c = data[i] & 255
+        if c >> 7 == 0:
             i += 1
             continue
         r = 0
-        if data[i] >> 3 == 30:
+        if c >> 3 == 30:
             r = 3
-        elif data[i] >> 4 == 14:
+        elif c >> 4 == 14:
             r = 2
-        elif data[i] >> 5 == 6:
+        elif c >> 5 == 6:
             r = 1
         else:
             return False
         while (r):
             r -= 1
             i += 1
-            if i == l or data[i] >> 6 != 2:
+            if i == l or (data[i] & 255) >> 6 != 2:
                 return False
     return True
