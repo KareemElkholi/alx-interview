0e6b306d4285146087e506e8d801322172bd6d51
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-07-14 18:31:21 +0300

    Correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-(async () => {
-  const film = await fetch(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`).then(res => res.json());
-  film.characters.map(async url => console.log(await fetch(url).then(res => res.json()).then(res => res.name)));
-})();
+const request = require('request');
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, { json: true }, (err, res, body) => {
+  if (err) return;
+  body.characters.forEach(url => request(url, { json: true }, (err, res, body) => {
+    if (err) return;
+    console.log(body.name);
+  }));
+});
